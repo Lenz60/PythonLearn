ef4a86f4c9537c397df9106dcc9e63372ac2ddcc
Lenz60 <raflyandrian@gmail.com>
2024-10-16 09:59:04 +0700

    feat(Main): Round up all the features to one input
    Rounded up all the feature to input select

--- Main.py
+++ Main.py
@@ -1,43 +1,73 @@
-# print("Hello world")
+import os
 import TempConversion as tc;
-test1 = 1
-test2 = 1.1
-test3 = "1"
-test4 = True
-# ? Casting data
-# data_int = 1;
-# data_float = float(data_int);
+import DatePrint as dp;
+# # print("Hello world")
+# test1 = 1
+# test2 = 1.1
+# test3 = "1"
+# test4 = True
+# # ? Casting data
+# # data_int = 1;
+# # data_float = float(data_int);
 
-# age = input("Input your age : \n")
-# print (age)
-# print(type(age))
-# print ("Casting age data to float")
-# print(type(float(age)))
+# # age = input("Input your age : \n")
+# # print (age)
+# # print(type(age))
+# # print ("Casting age data to float")
+# # print(type(float(age)))
 
 
-# print(test1)
-# print(test2)
-# print(test3)
-# print(test4)
+# # print(test1)
+# # print(test2)
+# # print(test3)
+# # print(test4)
 
-# print(type(test1))
-# print(type(test2))
-# print(type(test3))
-# print(type(test4))
+# # print(type(test1))
+# # print(type(test2))
+# # print(type(test3))
+# # print(type(test4))
 
-# if(type(test1) == int):
-#     print("This is an integer")
-    
-#  v Temperature Conversion
-# ? Clearing the screen first
-print(chr(27) + "[2J") 
-# ? Title
-print("Temperature Conversion")
-print("Select what temperature you want to convert")
-print("1. Celcius conversion")
-print("2. Reamur conversion")
-print("3. Fahrenheit conversion")
-print("4. Kelvin conversion")
-choice = input("Enter your choice : \n")
-# tc.decide_conversion(choice)
-tc.choice_of_temperatures(choice)
+# # if(type(test1) == int):
+# #     print("This is an integer")
+def featureSelect(choice):
+    match choice:
+        case "1":
+            print("Temperature Conversion")
+            print("Select what temperature you want to convert")
+            print("1. Celcius conversion")
+            print("2. Reamur conversion")
+            print("3. Fahrenheit conversion")
+            print("4. Kelvin conversion")
+            choice = input("Enter your choice : \n")
+            tc.choice_of_temperatures(choice)
+        case "2":
+            print("Enter your birthdate")
+            date = input("Enter your birthdate in format DD/MM/YYYY : \n")
+            dp.calculateDate(date)
+        case _:
+            print("Invalid choice")
+            exit()
+
+# print(chr(27) + "[2J") 
+os.system('cls')
+InputChoice = input("Select what you want to do : \n1. Temperature Conversion\n2. Date Calculation\n")
+featureSelect(InputChoice)
+
+# #  v Temperature Conversion
+# # ? Clearing the screen first
+
+# # ? Title
+# print("Temperature Conversion")
+# print("Select what temperature you want to convert")
+# print("1. Celcius conversion")
+# print("2. Reamur conversion")
+# print("3. Fahrenheit conversion")
+# print("4. Kelvin conversion")
+# choice = input("Enter your choice : \n")
+# # tc.decide_conversion(choice)
+# tc.choice_of_temperatures(choice)
+
+# print("Enter your birthdate")
+# date = input("Enter your birthdate in format DD/MM/YYYY : \n")
+# # dp.getBirthDate(date)
+# dp.calculateDate(date)
